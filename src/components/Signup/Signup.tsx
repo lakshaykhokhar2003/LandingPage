28dea4dcea9b81218db5438077e49838edb53b2b
Lakshay <lakshaykhokhar2003@gmail.com>
2024-02-27 23:38:25 +0530

    feat: added toastify for notification

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,8 +1,15 @@
-import classes from './Signup.module.scss';
+import {useRef} from "react";
 import {toast} from 'react-toastify';
+import classes from './Signup.module.scss';
 
 const Signup = () => {
+    const textRef = useRef<HTMLInputElement>(null);
     const handleSubmit = () => {
+        if (!textRef.current?.value || !textRef.current?.value.trim()) {
+            return toast.error('Please enter your email', {
+                autoClose: 2000
+            });
+        }
         toast.success('You have successfully signed up for our exclusive special deals', {
             autoClose: 2000
         });
@@ -10,7 +17,7 @@ const Signup = () => {
     return <div className={classes.Signup}>
         <h1>Sign up and get exclusive special deals</h1>
         <div className={classes.Input}>
-            <input type="text"/>
+            <input type="text" ref={textRef}/>
             <button onClick={handleSubmit}>Sign Up</button>
         </div>
     </div>
